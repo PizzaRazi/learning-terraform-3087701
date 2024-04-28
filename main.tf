64547c7f15bad456ec154b4792a202b686bdd55e
Jonathan Nguyen <74562467+PizzaRazi@users.noreply.github.com>
2024-04-28 12:13:12 -0700

    autoscalinggg

--- main.tf
+++ main.tf
@@ -33,19 +33,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
+  # insert the 1 required variable here
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_groups_arns
+  security_groups     = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name            = "blog-alb"
@@ -53,68 +57,9 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  # Security Group
-  # security_group_ingress_rules = {
-  #   all_http = {
-  #     from_port   = 80
-  #     to_port     = 80
-  #     ip_protocol = "tcp"
-  #     description = "HTTP web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  #   all_https = {
-  #     from_port   = 443
-  #     to_port     = 443
-  #     ip_protocol = "tcp"
-  #     description = "HTTPS web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  # }
-  # security_group_egress_rules = {
-  #   all = {
-  #     ip_protocol = "-1"
-  #     cidr_ipv4   = "10.0.0.0/16"
-  #   }
-  # }
-
   access_logs = {
     bucket = "my-alb-logs"
   }
-
-  #   listeners = {
-  #     ex-http-https-redirect = {
-  #       port     = 80
-  #       protocol = "HTTP"
-  #       redirect = {
-  #         port        = "443"
-  #         protocol    = "HTTPS"
-  #         status_code = "HTTP_301"
-  #       }
-  #     }
-  #     ex-https = {
-  #       port            = 443
-  #       protocol        = "HTTPS"
-  #       certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-  # 
-  #       forward = {
-  #         target_group_key = "ex-instance"
-  #       }
-  #     }
-  #   }
-
-  # target_groups = [
-  #   {
-  #     name_prefix = "blog-"
-  #     protocol    = "HTTP"
-  #     port        = 80
-  #     target_type = "instance"
-  #     # target = {
-  #     #   my-target = {
-  #     #     target_id = aws_instance.blog.id
-  #     #   }
-  #     # }
-  #   }
-  # ]
 
   target_groups = {
     ex-instance = {
@@ -125,8 +70,6 @@ module "alb" {
       target_id   = aws_instance.blog.id
     }
   }
-
-
 
   tags = {
     Environment = "Development"
