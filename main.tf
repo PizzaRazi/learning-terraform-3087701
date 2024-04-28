f7f9274ae2adb4ed8473db6444617656cad928be
Jonathan Nguyen <74562467+PizzaRazi@users.noreply.github.com>
2024-04-28 09:46:06 -0700

    test reference

--- main.tf
+++ main.tf
@@ -24,7 +24,8 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -33,19 +34,25 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.0"
+  # insert the 1 required variable here
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name            = "blog-alb"
@@ -54,67 +61,40 @@ module "alb" {
   security_groups = [module.blog_sg.security_group_id]
 
   # Security Group
-  # security_group_ingress_rules = {
-  #   all_http = {
-  #     from_port   = 80
-  #     to_port     = 80
-  #     ip_protocol = "tcp"
-  #     description = "HTTP web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  #   all_https = {
-  #     from_port   = 443
-  #     to_port     = 443
-  #     ip_protocol = "tcp"
-  #     description = "HTTPS web traffic"
-  #     cidr_ipv4   = "0.0.0.0/0"
-  #   }
-  # }
-  # security_group_egress_rules = {
-  #   all = {
-  #     ip_protocol = "-1"
-  #     cidr_ipv4   = "10.0.0.0/16"
-  #   }
-  # }
-
-  access_logs = {
-    bucket = "my-alb-logs"
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
   }
 
-  #   listeners = {
-  #     ex-http-https-redirect = {
-  #       port     = 80
-  #       protocol = "HTTP"
-  #       redirect = {
-  #         port        = "443"
-  #         protocol    = "HTTPS"
-  #         status_code = "HTTP_301"
-  #       }
-  #     }
-  #     ex-https = {
-  #       port            = 443
-  #       protocol        = "HTTPS"
-  #       certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-  # 
-  #       forward = {
-  #         target_group_key = "ex-instance"
-  #       }
-  #     }
-  #   }
-
-  # target_groups = [
-  #   {
-  #     name_prefix = "blog-"
-  #     protocol    = "HTTP"
-  #     port        = 80
-  #     target_type = "instance"
-  #     # target = {
-  #     #   my-target = {
-  #     #     target_id = aws_instance.blog.id
-  #     #   }
-  #     # }
-  #   }
-  # ]
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
 
   target_groups = {
     ex-instance = {
@@ -126,20 +106,19 @@ module "alb" {
     }
   }
 
-
-
   tags = {
-    Environment = "Development"
+    Environment = "dev"
     Project     = "Example"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-  name    = "blog"
+  version = "5.1.0"
+
 
   vpc_id = module.blog_vpc.vpc_id
+  name   = "blog"
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
